7b4141975100aa23fca94600d8b4d59b99ed8581
Fredy Reyes <freyes-2019035@kinal.edu.gt>
2020-08-13 16:15:40 -0600

    boton para subir y estilos de imagen

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+/*Js made by Fredy Reyes 2019035*/
+
+jQuery('document').ready(function($){
+    var subir = $('.back-to-top');
+    subir.click(function(e){
+      e.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 500);
+    });  
+    subir.hide();
+    
+    $(window).scroll(function(){
+      
+      if( $(this).scrollTop() > 200 ) {
+        subir.fadeIn();
+      } else {
+        subir.fadeOut();
+      }
+      
+    });  
+  });
