12f1b7e1a6ea5f78793d38fd6bda33ed02938afb
freyes2019035 <freyes-2019035@kinal.edu.gt>
2020-08-19 12:23:04 -0600

    some bugs fixed

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,30 +1,23 @@
 /*Js made by Fredy Reyes 2019035*/
-
-jQuery('document').ready(function($){
-    var subir = $('.back-to-top');
-    subir.click(function(e){
-      e.preventDefault();
-      $('html, body').animate({scrollTop: 0}, 500);
-    });  
-    subir.hide();
-    
-    $(window).scroll(function(){
-      
-      if( $(this).scrollTop() > 200 ) {
-        subir.fadeIn();
-      } else {
-        subir.fadeOut();
-      }
-      
-    });  
+jQuery('document').ready(function($) {
+  var subir = $('.back-to-top');
+  subir.click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500);
   });
-
-//Preloader
-
-  window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden";
+  subir.hide();
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      subir.fadeIn();
+    } else {
+      subir.fadeOut();
+    }
+  });
 });
-
-
-
+//Preloader
+window.addEventListener("load", function() {
+  const loader = document.querySelector(".loader");
+  loader.className += " hidden";
+});
